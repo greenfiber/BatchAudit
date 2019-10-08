06d536ee936406a06f5cf99e27c86149d0462c18
David J. Fries <david.fries@greenfiber.com>
2019-10-08 17:51:05 -0400

    updated output to have requested additional info

--- apbatchaudit.py
+++ apbatchaudit.py
@@ -27,6 +27,29 @@ def getbatchesfromdb():
     for row in rows:
         data.append(row)
     return data
+def getbatchinfo(batchno):
+    query= '''
+   SELECT  
+       [UDF_BATCH_NO],
+	   TRANSACTIONDATE,
+	   VENDORNO,
+	   INVOICENO,
+	   HEADERSEQNO
+     
+        FROM [MAS_GFC].[dbo].[AP_INVOICEHISTORYHEADER]
+        where UDF_BATCH_NO =?
+        order by TRANSACTIONDATE desc
+    
+    '''
+    cursor = cx.cursor()
+    
+    
+    cursor.execute(query,batchno)
+    rows = cursor.fetchone()
+#     data=[]
+#     for row in rows:
+#         data.append(row)
+    return rows
 print("Generating batch file list...")
 batchfiles = [f for f in listdir('L:\\APBatches') if isfile(join('L:\\APBatches',f)) and f.endswith('.pdf')]
 print("Generating POBatches list...")
@@ -43,6 +66,25 @@ for row in rows:
     db.append(str(row.UDF_BATCH_NO))
 missing=set(db)-set(filenames)
 missing-=set(pofilenames)
+data={
+    "batchno":"",
+    "transactiondate":"",
+    "vendorno":"",
+    "invoiceno":"",
+    "headerseqno":""
+    
+}
+extra=[]
+for rec in missing:
+    batch=getbatchinfo(rec)
+    data['batchno']=batch.UDF_BATCH_NO
+    data['vendorno']=batch.VENDORNO
+    data['invoiceno']=batch.INVOICENO
+    data['headerseqno']=batch.HEADERSEQNO
+    data['transactiondate']=batch.TRANSACTIONDATE
+#     print(data)
+    extra.append(data.copy())
+# print(extra)
 df = pd.DataFrame(missing)
 wb= xw.Book()
 sheet= wb.sheets['Sheet1']
